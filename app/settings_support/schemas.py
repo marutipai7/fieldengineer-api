8622fb63ab8abce8b37b5f70b0cd50eb5fc3a7da
ayush1506shukla <ayushshukla1506@gmail.com>
2026-09-03 12:39:07 +0530

    permission entire field is available

--- app/settings_support/schemas.py
+++ app/settings_support/schemas.py
@@ -7,9 +7,15 @@ class ContactSupportSchema(BaseModel):
 
 class PermissionUpdateSchema(BaseModel):
     location: Optional[bool] = None
-    camera: Optional[bool] = None
-    microphone: Optional[bool] = None
+    communication: Optional[bool] = None
     notifications: Optional[bool] = None
+    camera: Optional[bool] = None
+    media: Optional[bool] = None
+    audio: Optional[bool] = None
+    payment: Optional[bool] = None
+    security: Optional[bool] = None
+    network: Optional[bool] = None
+    device: Optional[bool] = None
 
 class ContactSupportSchema(BaseModel):
     subject: str
